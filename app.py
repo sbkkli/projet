ac26313f4709bdf4321d60ecf46281149b450b1a
sbkkli <sara.bekkali@uit.ac.ma>
2025-12-11 15:25:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 from sqlalchemy import create_engine, text
 
-DATABASE_URL = "mysql+pymysql://root:Papa7777@@localhost/projetbd"   # adapte les valeurs
+DATABASE_URL = "mysql+pymysql://root:Papa7777%40@localhost/projetbd"   # adapte les valeurs
 engine = create_engine(DATABASE_URL)
 with engine.connect() as conn:
     result = conn.execute(text("SHOW TABLES;"))
     print(result.fetchall())
+
 
 
 
